61c5fedd69a19eaba53eed614977d0f84797deda
srbony <srabonycse@gmail.com>
2022-11-10 10:17:06 +0600

    added delete api

--- index.js
+++ index.js
@@ -61,7 +61,16 @@ async function run() {
             const cursor = reviewCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
-        })
+        });
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
     }
     finally {
 
